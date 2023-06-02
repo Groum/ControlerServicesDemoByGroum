f93e4d8f02d3ad522ddb3295a2c8459bc676aa5e
groum <groums@yahoo.com>
2023-06-02 01:10:24 -0400

    File Renmae to TechWish

--- H2implementation/src/main/java/com/example/H2implementation/AccessSecurity.java
+++ H2implementation/src/main/java/com/example/H2implementation/AccessSecurity.java
@@ -1,17 +1,13 @@
 package com.example.H2implementation;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AccessSecurity  {
 
     @Autowired
-        private TekWishUserDetailsService userDetailsService;
+        private TechWishUserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- H2implementation/src/main/java/com/example/H2implementation/TechWishUserDetailsService.java
+++ H2implementation/src/main/java/com/example/H2implementation/TechWishUserDetailsService.java
@@ -1,15 +1,12 @@
 package com.example.H2implementation;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-
 @Service
-public class TekWishUserDetailsService implements UserDetailsService {
+public class TechWishUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
